6ed31e3b87ddd6bf3bc3fc96ba841a056aa158ab
Anton Prince Oommen <96407588+antonnprince@users.noreply.github.com>
2024-06-19 18:30:50 +0530

    Create dummy.js

---
+++ src/dummy.js
@@ -0,0 +1,28 @@
+let data = [
+    {
+        from:"Cochin International Airport",
+        region:"Cochin",
+        airlines:[
+            {
+                company:"Indigo",
+                fare_adult:10,
+                fare_children:5
+            },
+            
+            {
+                company:"Vistara",
+                fare_adults:15,
+                fare_children:7    
+            }
+        ],
+        trip:"Direct",
+        tod:"some time",
+        toa:"time"
+    },
+
+    {
+        to:"Delhi"
+    }
+]
+
+export {data}

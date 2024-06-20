00334eaf40af950859e0c6297dc817803c98fab7
Anton Prince Oommen <96407588+antonnprince@users.noreply.github.com>
2024-06-20 11:08:04 +0530

    toast message implement

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -2,29 +2,61 @@ import { useState } from "react";
 import { useContext,createContext } from "react";
 import {useNavigate} from "react-router-dom"
 import axios from "axios";
+import { ToastContainer, toast } from 'react-toastify'
 
 const AuthContext = createContext()
 
 export const AuthProvider=({children})=>{
 
-    const [user,setUser]=useState()
+    const API_KEY="et4dZOAoHASe1IUb5F3AjWenAmZ9AY5j"
+    const API_SECRET="dqJwpL2Cqnsampuy"
+    const [user,setUser]=useState({})
     const navigate=useNavigate()
+
+    const getToken=async(clientId, clientSecret)=>{
+        try 
+        {
+          const res = await axios.post(
+            'https://test.api.amadeus.com/v1/security/oauth2/token',
+          `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`,
+           {
+              headers: 
+              {
+                'Content-Type': 'application/x-www-form-urlencoded',
+              },
+            }
+          )
+          return res
+        } 
+        catch (error) 
+        {
+            console.log(error)
+        }
+      }
+
 
     const login=async(data)=>{
         try 
         {
-            
             const res = await axios.post("http://localhost:3001/login",
-                {email:data.email, password: data.password})    
+                {email:data.email, password: data.password})  
+
             if(res.status===200)
-                {
+                {   
+                    const result = await getToken(API_KEY,API_SECRET)
+                    data["token"]=result.data.access_token
                     setUser(data)
+                    console.log(user)
                     navigate('/profile')
-                    return res
                 }
                else
                {
-                alert(res)
+                toast.error(res.data.message,{
+                    position: "top-center", 
+                    autoClose: 2000, 
+                    pauseOnHover:false,
+                    theme:"dark"
+                  })
                }
             return res
         } 
@@ -43,6 +75,7 @@ export const AuthProvider=({children})=>{
     return(
         <AuthContext.Provider value={{login,logout,user}}>
             {children}
+            <ToastContainer/>
         </AuthContext.Provider>
     )
 }

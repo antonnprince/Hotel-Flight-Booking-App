6b8a8caf6ca2124e4ae24b44472fca389fcc6394
Anton Prince Oommen <96407588+antonnprince@users.noreply.github.com>
2024-06-21 18:13:28 +0530

    Update Hotels.jsx

--- src/Hotels.jsx
+++ src/Hotels.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import { useAuth } from './AuthContext'
+import room from "./room.jpg"
+
+
 const Hotels = () => {
 
 const API_KEY="470f24fdfaa0e1078cc6433c8eafde5ba8767b81e09fd35146acae47ac238988"
@@ -11,7 +14,7 @@ const [hotels, setHotels] = useState()
 
 const getHotels=async()=>
 {
-    const res = await axios.get("https://test.api.amadeus.com/v1/reference-data/locations/hotels/by-city?cityCode=COK&radius=25&radiusUnit=KM&amenities=SWIMMING_POOL,SPA,FITNESS_CENTER,CASINO,JACUZZI,MASSAGE,VALET_PARKING,BAR,MINIBAR,ROOM_SERVICE&hotelSource=ALL",
+    const res = await axios.get("https://test.api.amadeus.com/v1/reference-data/locations/hotels/by-city?cityCode=COK&radius=25&radiusUnit=KM&amenities=SWIMMING_POOL,SPA,FITNESS_CENTER,AIR_CONDITIONING,CASINO,JACUZZI,MASSAGE,BAR,MINIBAR&hotelSource=ALL",
         {
             headers:{
                 'Authorization': `Bearer ${user.token}`,
@@ -29,29 +32,57 @@ function getRandom(){
 function getPrice(){
     return Math.floor(Math.random()*5000)
 }
+
+function getWord(word) {
+
+     // Replace underscores with spaces and convert the word to lowercase with the first letter in uppercase
+    let formattedWord = word.replace(/_/g, ' ').toLowerCase();
+    return formattedWord.charAt(0).toUpperCase() + formattedWord.slice(1);
+  }
+  
     return (
-    <div>
+    <div className='mx-auto'>
         <button onClick={getHotels}>
             Hotels
         </button>
         
-        {
-            hotels && (
+        {   hotels? 
+             (
                 hotels.map((hotel,index)=>{
                     const rating = getRandom()
                     const price = getPrice()
                 return(
-                        <div key={index} className='bg-neutral-400 p-4 rounded-xl my-4 mx-auto'>
-                            <h2>{hotel.name}</h2>
+                        <div key={index} className='bg-neutral-400 mx-auto p-4 flex flex-col h-fit rounded-xl my-4'>
+                            
+                            <img src={room} className='w-1/3 h-1/2'/>
+                            
+                            <div className=''>
+                                <h2 className='mx-6 font-bold text-xl'>{hotel.name}</h2>
+                            </div>
+                            
+                            <div className=''>
+                                {
+                                    hotel.amenities.map((item,index)=>
+                                        {   
+                                            const res = getWord(item)
+                                            return(
+                                            <h2 key={index}>{res}</h2>
+                                            )
+                                        })
+                                }
+                            </div>
+                            
+                            <h2>₹{price} Per Night</h2>
                             {
-                                hotel.amenities.map((item,index)=><h2 key={index}>{item}</h2>)
+                                  Array(rating).fill().map((_, i)=>
+                                (
+                                    <span key={i}>⭐</span>
+                                ))
                             }
-                            <h2>{price} Per Night</h2>
-                            <h2>{rating} stars</h2>
                         </div>
                     )
                 })
-            )
+            ):<span class="animate-ping absolute inline-flex h-4 w-4 rounded-full bg-sky-400 "></span>
         }
     </div>
   )

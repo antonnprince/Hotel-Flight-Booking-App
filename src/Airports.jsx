813a2c55b47309c3218d96ad874f5a025f6b1be2
Anton Prince Oommen <96407588+antonnprince@users.noreply.github.com>
2024-06-21 12:35:49 +0530

    design

--- src/Airports.jsx
+++ src/Airports.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { useAuth } from './AuthContext'
+import flight from './fligt.png'
 
 const Airports = () => {
 
@@ -45,7 +46,6 @@ const Airports = () => {
       className='bg-indigo-400 m-8 text-xl p-2 rounded-full'>
           Get Flights
       </button>
-      
         { 
           data&&
               ( 
@@ -60,21 +60,29 @@ const Airports = () => {
                     })
                     const airlines = getCarrier(item.validatingAirlineCodes[0])
                     return(         
-                          <div className='mx-4 text-xl flex flex-row space-x-8 font-semibold text-white bg-neutral-500 my-4 rounded-xl p-4 '
+                          <div className='mx-4 text-xl flex flex-row space-x-8 font-semibold text-slate-950 bg-stone-50
+                          border-2 border-stone-300 shadow-xl
+                          my-4 rounded-xl p-4 w-auto '
                            key={index}>
-                            <h2 className='my-auto'>{airlines}</h2>
-                              <div className='flex flex-col my-2'>
-                                <h2 className='mr-24'>{item.itineraries[0].segments[0].departure.at.substring(11).slice(0,-3)}</h2>  
-                                <h4 className='mr-24'>{item.itineraries[0].segments[0].departure.iataCode} </h4>
-                                <h2 className='text-right'>{item.itineraries[0].duration.substring(2).toLowerCase()} </h2>
+                           
+                              <div className='flex flex-col space-y-2'>
+                                <img src={flight} className=' w-8 h-8 sm:w-12 sm:h-12  md:w-24 md:h-24'/>
+                                <h2 className='m-auto font-normal text-lg md:text-xl'>{airlines}</h2>
                               </div>
-                              
-                              <div className='flex flex-col my-2'>
-                                <h2 >{res.at.substring(11).slice(0,-3)}</h2>
-                                <h4 >{res.iataCode} </h4>
+
+
+                              <div className='flex flex-row space-x-2 md:space-x-8 lg:space-x-12'>
+                                  <div className='flex flex-col text-2xl '>
+                                    <h2 className='font-extrabold text-2xl lg:text-3xl mx-auto '>{item.itineraries[0].segments[0].departure.at.substring(11).slice(0,-3)}</h2>  
+                                    <h4 className='text-lg lg:text-xl mx-auto'>{item.itineraries[0].segments[0].departure.iataCode} </h4>
+                                  </div>
+                                  <h2 className='text-lg lg:text-lg font-normal mt-20'>{item.itineraries[0].duration.substring(2).toLowerCase()} </h2>
+                                  <div className=' flex flex-col mx-auto'>
+                                    <h2 className='font-extrabold text-2xl lg:text-3xl'>{res.at.substring(11).slice(0,-3)}</h2>
+                                    <h4 className='text-lg lg:text-xl mx-auto'>{res.iataCode} </h4>
+                                  </div>
                               </div>
-                                <h2 className='text-3xl my-auto'>₹{item.price.total.slice(0,-3)}</h2>
-                               
+                                <h2 className='text-2xl lg:text-3xl my-auto'>₹{item.price.total.slice(0,-3)}</h2>
                           </div>                      
                       )
                     }
